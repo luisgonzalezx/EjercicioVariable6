47bbe494ab23472c9a78bb75ea9f03c37d579be4
luis <lagonzalez1688@gmail.com>
2020-10-13 00:49:45 -0700

    Ejercicio variable 6

---
+++ src/ejerciciovariable6/Ejerciciovariable6.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciovariable6;
+
+public class Ejerciciovariable6 {
+
+    
+    public static void main(String[] args) {
+        
+        int c=55;
+        
+        if(c>=0) {
+            
+            System.out.println("es número positivo");
+        }
+        if(c%2!=0){
+            System.out.println("es número impar");
+        }
+        if(c%5==0){
+            
+          System.out.println("es múltiplo de 5");
+        }
+    
+         if(c%10==0){
+            
+            System.out.println("el número es múltiplo de 10");
+        }
+        
+        if(c>100){
+            
+            System.out.println("el número es menor que 100");
+        }
+    }
+}
